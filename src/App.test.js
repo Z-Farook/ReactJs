b5c1967812440cb785fc310f36cc880aef75c7b1
zahid.farook <zahid.farook@cgi.com>
2022-08-04 09:09:52 +0200

    add items in src folder

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let component;
+
+beforeEach(() => {
+  component = shallow(<App />);
+});
+
+it("renders without crashing", () => {
+  expect(component.text()).toEqual(
+    "Welcome to React Bookstore<ProductList /><Cart />"
+  );
 });
